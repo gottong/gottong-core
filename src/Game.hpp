fc28ee9ba8ccbb1e41bffc9d551d1386e4bea083
asdefary dea <asdefary@gmail.com>
2013-08-27 23:44:48 +0900

    Starting Sources

---
+++ src/Game.hpp
@@ -0,0 +1,12 @@
+#ifndef __GOTTONG_GAME_HPP__
+#define __GOTTONG_GAME_HPP__
+
+namespace Gottong {
+
+class Game {
+};
+
+} /* namespace Gottong */
+
+#endif /* __GOTTONG_GAME_HPP__ */
+
---
+++ src/Gottong.hpp
@@ -0,0 +1,14 @@
+#ifndef __GOTTONG_HPP__
+#define __GOTTONG_HPP__
+
+#include "Game.hpp"
+
+namespace Gottong {
+
+void Init();
+int Run(Game game);
+
+} /* namespace Gottong */
+
+#endif /* __GOTTONG_HPP__ */
+
