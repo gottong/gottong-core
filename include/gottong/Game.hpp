5b9243c2801d8fdcfca65f4274f642c040976053
asdefary dea <asdefary@gmail.com>
2013-08-28 15:32:03 +0900

    Add Resource.hpp

--- include/gottong/Game.hpp
+++ include/gottong/Game.hpp
@@ -6,6 +6,7 @@
 #include <memory>
 
 #include "GameObject.hpp"
+#include "Resource.hpp"
 
 namespace Gottong {
 
---
+++ include/gottong/Resource.hpp
@@ -0,0 +1,18 @@
+#ifndef __GOTTONG_RESOURCE_HPP__
+#define __GOTTONG_RESOURCE_HPP__
+
+#include <memory>
+
+#include "GameObject.hpp"
+
+namespace Gottong {
+
+class Resource {
+public:
+	std::shared_ptr<GameObject> instantiate() = 0;
+};
+
+} /* namespace Gottong */
+
+#endif /* __GOTTONG_RESOURCE_HPP__ */
+
